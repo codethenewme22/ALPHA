48671bfb194cef224c90227541bd97ad556ecc53
CDNM22 <codethenewme22@gmail.com>
2022-08-05 17:35:13 +0200

    sum

--- Class-1/cl1_6_SumOfaAndb.java
+++ Class-1/cl1_6_SumOfaAndb.java
@@ -5,7 +5,11 @@
 
 public class cl1_6_SumOfaAndb {
   public static void main(String[] args){
-    System.out.println(" ");
+    int a =10;
+    int b =15;
+    int sum = (a+b);
+
+    System.out.println(sum);
 
   }
 }
