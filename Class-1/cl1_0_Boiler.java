e5f747cb72e3fafa6711e6a91c77a60858981ed0
CDNM22 <codethenewme22@gmail.com>
2022-08-05 19:36:46 +0200

    8 is doen

--- Class-1/cl1_0_Boiler.java
+++ Class-1/cl1_0_Boiler.java
@@ -1,11 +1,27 @@
 /*
  
 
- */
+ 
 
 public class cl1_0_Boiler {
-  public static void main(String[] args){
+  public static void main(String args[]){
     System.out.println(" ");
+
+  }
+}
+*/
+
+import java.util.*; 
+public class cl1_0_Boiler {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in); 
+    
+    System.out.print("Please enter a the TRUTH: ");
+    Boolean Bull= sc.nextBoolean();
+    System.out.println(Bull);
+
+    
+    
 
   }
 }
--- Class-1/cl1_8_InputInJava.java
+++ Class-1/cl1_8_InputInJava.java
@@ -6,11 +6,45 @@
  * import java.util.*;
 
  */
-import java.util.*;
+import java.util.*; // This will import the UTILITY files which will allow us to give INPUT from the USER
 public class cl1_8_InputInJava {
-  public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    System.out.println(" ");
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);  // Scanner> then a variable name>  and then new Scanner(System.in); 
 
+
+    // todo INPUT without SPACE:
+        System.out.print("Please enter a anything without spaces: ");
+
+    String Name= sc.next();  // This > next() < CAPTURES the INPUT from the USER & > String < STORES the INPUT as STRING.
+    // ! > next() < only can't recognize SPACE!! So, it can take any number of characters unless a SPACE 
+    System.out.println(Name);
+    
+    
+    // todo INPUT with SPACE:
+    System.out.print("Please enter a anything with spaces: ");
+    String FullName= sc.nextLine(); 
+    System.out.println(FullName);
+
+
+    // todo INPUT NUMBER:
+    System.out.print("Please enter a Number: ");
+    int number= sc.nextInt();
+    System.out.println(number);
+
+
+    // todo Different TYPEs of INPUT (integer, characters, line ) requires different types of FUNCTIONS.
+    /*
+     * next()           is for Characters WITHOUT space
+     * nextLine()       is for Characters WITH space
+     * nextInt()        is for INTEGER
+     * nextFloat()      is for FLOAT
+     * nextDouble()     is for DOUBLE
+     * nextFBoolean()   is for BOOLEAN
+     * nextShort()      is for SHORT
+     * nextLong()       is for LONG
+     *
+     * Scanner sc = new Scanner(System.in); 
+     *  float price= sc.nextFloat();
+     */
   }
 }
