1d7ae8595c7511d8be7c452710609e50415125f1
CDNM22 <codethenewme22@gmail.com>
2022-08-05 23:23:45 +0200

    1

--- Class-1/cl1_8_InputInJava.java
+++ Class-1/cl1_8_InputInJava.java
@@ -11,14 +11,15 @@ public class cl1_8_InputInJava {
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);  // Scanner> then a variable name>  and then new Scanner(System.in); 
 
-
+/* 
     // todo INPUT without SPACE:
         System.out.print("Please enter a anything without spaces: ");
 
     String Name= sc.next();  // This > next() < CAPTURES the INPUT from the USER & > String < STORES the INPUT as STRING.
     // ! > next() < only can't recognize SPACE!! So, it can take any number of characters unless a SPACE 
     System.out.println(Name);
-    
+
+    */
     
     // todo INPUT with SPACE:
     System.out.print("Please enter a anything with spaces: ");
--- Class-1/tempCodeRunnerFile.java
+++ Class-1/tempCodeRunnerFile.java
@@ -1,1 +1,7 @@
-DATA TYPES in Java
+
+    // // todo INPUT without SPACE:
+    //     System.out.print("Please enter a anything without spaces: ");
+
+    // String Name= sc.next();  // This > next() < CAPTURES the INPUT from the USER & > String < STORES the INPUT as STRING.
+    // // ! > next() < only can't recognize SPACE!! So, it can take any number of characters unless a SPACE 
+    // System.out.println(Name);
