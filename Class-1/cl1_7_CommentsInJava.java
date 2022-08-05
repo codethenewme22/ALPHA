07d0395bcb3e37b90f740bbeb856ed6ec107bca7
CDNM22 <codethenewme22@gmail.com>
2022-08-05 17:46:07 +0200

    8

---
+++ Class-1/cl1_7_CommentsInJava.java
@@ -0,0 +1,19 @@
+/*
+ 
+
+ */
+
+public class cl1_7_CommentsInJava {
+  public static void main(String[] args){ 
+    System.out.println(" "); // This it the comment
+
+  }
+}
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
---
+++ Class-1/cl1_8_InputInJava.java
@@ -0,0 +1,16 @@
+/*
+ ! How to take INPUT from the USER:
+ ? A class has to be created to TAKE INPUT from the user: 
+ * Scanner
+ ? This header should be put in the beginning:
+ * import java.util.*;
+
+ */
+import java.util.*;
+public class cl1_8_InputInJava {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(" ");
+
+  }
+}
