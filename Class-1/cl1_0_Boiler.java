38129f347cdf61430a65b25fe5fcd8d2da39ba80
CDNM22 <codethenewme22@gmail.com>
2022-08-05 20:23:18 +0200

    9 is done

--- Class-1/cl1_0_Boiler.java
+++ Class-1/cl1_0_Boiler.java
@@ -1,27 +1,45 @@
 /*
- 
+
 
  
 
-public class cl1_0_Boiler {
+
   public static void main(String args[]){
     System.out.println(" ");
+  }
+
+
+
+import java.util.*; 
+// import java.io.*;
+public class cl1_0_Boiler {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in); 
+    
+    System.out.print(" Please enter first number: ");
+float a = sc.nextFloat();
+System.out.print(" Please enter second number: ");
+float b = sc.nextFloat();
+float sum = a+b;
+System.out.println(" Total of the Numbers is: "+sum);
+    
+    
 
   }
 }
 */
 
 import java.util.*; 
-public class cl1_0_Boiler {
-  public static void main(String args[]){
-    Scanner sc = new Scanner(System.in); 
+public class cl1_0_Boiler
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
     
-    System.out.print("Please enter a the TRUTH: ");
-    Boolean Bull= sc.nextBoolean();
-    System.out.println(Bull);
-
-    
-    
-
-  }
+    System.out.print(" Please enter first number: ");
+float a = sc.nextFloat();
+System.out.print(" Please enter second number: ");
+float b = sc.nextFloat();
+float sum = a+b;
+System.out.println(" Total of the Numbers is: "+sum);
+    }
 }
---
+++ Class-1/cl1_9_SumOfaAndb.java
@@ -0,0 +1,19 @@
+ /* 
+  ! 8. SUM of a & B using SCANNER object:
+
+
+ */
+import java.util.*;
+public class cl1_9_SumOfaAndb {
+  public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+
+
+    System.out.print(" Please enter first number: ");
+int a = sc.nextInt();
+System.out.print(" Please enter second number: ");
+int b = sc.nextInt();
+int sum = a+b;
+System.out.println(" Total of the Numbers is: "+sum);
+  }
+}
