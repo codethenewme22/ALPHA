af47fe512a143fd1da9f361672bafdecca7ec290
CDNM22 <codethenewme22@gmail.com>
2022-08-12 02:02:54 +0200

    cl_2_2 is done

---
+++ Class-2/cl2_2_ArithmeticOperators.java
@@ -0,0 +1,26 @@
+/*
+ ! 2.2 ARITHMETIC Operators: 
+
+ todo BINARY Operators: Where we need  TWO OPERANDS
+ todo UNARY Operators: Where we need  ONE OPERAND
+
+ ? BINARY           UNARY
+ *  +                 ++ 
+ *  -                 --
+ *  *                 
+ *  /                 
+ *  %                 
+
+ */
+public class cl2_2_ArithmeticOperators {
+  public static void main (String args []){
+    int a= 10;
+    int b= 5;
+    System.out.println("Add = "+ (a+b));
+    System.out.println("Subtract = "+ (a-b));
+    System.out.println("Multiply = "+ (a*b));
+    System.out.println("Divide = "+ (a/b));
+    System.out.println("Reminder = "+ (a%b));
+    
+      }
+}
