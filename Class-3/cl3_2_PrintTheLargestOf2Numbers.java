7bc5f0095f426a8f16876f93da2327154e82a5ef
CDNM22 <codethenewme22@gmail.com>
2022-08-13 03:20:31 +0200

    cl3_2 is done

--- Class-3/cl3_2_PrintTheLargestOf2Numbers.java
+++ Class-3/cl3_2_PrintTheLargestOf2Numbers.java
@@ -1,3 +1,32 @@
+/*
+! 3.2 PRINT THE LARGEST OF 2 NUMBERS:
+A= 1;
+B= 3;
+ */
+
+ import java.util.*;
 public class cl3_2_PrintTheLargestOf2Numbers {
+public static void main (String args []){
+ Scanner sc = new Scanner(System.in);
+ System.out.print("Please enter NUMBER ONE:");
+ int a=sc.nextInt();
+ System.out.print("Please enter NUMBER TWO:");
+ int b=sc.nextInt();
+
+if (a>b){
+  System.out.println("NUMBER ONE is GREATER");
+}
+else if (a<b){
+  System.out.println("NUMBER TWO is GREATER");
+}
+else if (a==b){
+  System.out.println("Both NUMBERS are SAME.");
+}
+else{
+
+  System.out.println("NUMBER ONE is GREATER");
+}
+
+}
   
 }
