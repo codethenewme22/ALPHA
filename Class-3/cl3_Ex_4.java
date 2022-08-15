e711d64317ad369792175b285d89e7a199fa73ef
CDNM22 <codethenewme22@gmail.com>
2022-08-15 23:53:45 +0200

    cl3_Ex_4 is done

---
+++ Class-3/cl3_Ex_4.java
@@ -0,0 +1,15 @@
+
+/*
+ ! What will be the VALUE of x & y in the following:
+ */
+
+public class cl3_Ex_4 {
+  public static void main (String args[]) {
+int a= 63, b= 36;
+boolean x= (a<b) ? true: false;
+System.out.println(x);
+int y = (a>b) ? a:b;
+System.out.println(y);
+
+  }
+}
