2d5f7f3d9574b4a86427ab977b928dc035ff1fa8
CDNM22 <codethenewme22@gmail.com>
2022-08-16 19:01:49 +0200

    till 4.3 is done

--- C-4 Loops (Flow Control)/cl4_2_WhileLoop.java
+++ C-4 Loops (Flow Control)/cl4_2_WhileLoop.java
@@ -1,5 +1,27 @@
+/*
+ ! 4.2 WHILE LOOP:
+ while (condition){
 
+  do something
+ }
+ */
 
+ import java.util.*;
 public class cl4_2_WhileLoop {
+  public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
+System.out.println("Whatever you will write, it will be echoed 100 times.");
+System.out.print("Please enter TEXT as you wish: ");
+String text= sc. nextLine();
+int counter=0;
+while(counter<100000){
+    System.out.println(text);
+    counter++;
+    
+  }
+  System.out.println(text+" was printed 100  times");
+
+  }
+
   
 }
