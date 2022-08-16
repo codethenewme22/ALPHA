55e002ea7d1be78e4a4b5ba5cc7afe46309b3a7e
CDNM22 <codethenewme22@gmail.com>
2022-08-16 15:17:29 +0200

    cl4_1_LoopsBasics is done

--- C-4 Loops (Flow Control)/cl4_1_LoopsBasics.java
+++ C-4 Loops (Flow Control)/cl4_1_LoopsBasics.java
@@ -1,1 +1,17 @@
+/*
+ ! 4.1 LOOPS BASICS: If we wanna do a specific task repeatedly or for huge number of times, we use LOOPS in Programming.
+ ? 3 TYPES of LOOPS:
+ * FOR loop
+ * WHILE loop
+ * DO WHILE loop
+ */
 
+ import java.util.*;
+public class cl4_1_LoopsBasics{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter a W")
+    char m = sc.nextChar();
+
+  }
+}
