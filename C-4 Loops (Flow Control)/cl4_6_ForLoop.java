9cba3561cc71a68a310db2ab6252288022078c64
CDNM22 <codethenewme22@gmail.com>
2022-08-23 01:09:30 +0200

    cl4_6_ForLoop is done

---
+++ C-4 Loops (Flow Control)/cl4_6_ForLoop.java
@@ -0,0 +1,25 @@
+/*
+ ! 4.5 FOR LOOP:
+
+ FOR LOOP brings 3 LINED STATEMENTS of WHILE LOOP in to 1 LINE 
+ * Syntax: 
+ for (Initialization; condotion; updation)
+ {
+  statements
+ }
+
+ * Example:
+ * for ()
+ */
+
+public class cl4_6_ForLoop {
+  public static void main(String[] args){
+// int i=1;
+    for(int i=1; i<= 5; i++) {
+
+      System.out.println("Hello World!");
+    }
+  
+  }
+  
+}
