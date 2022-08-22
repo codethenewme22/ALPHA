28a086f5a0f592b922dda7a0b5b10107b4759aec
CDNM22 <codethenewme22@gmail.com>
2022-08-23 01:57:21 +0200

    cl4_6_PrintReverseOfANumber is done

--- C-4 Loops (Flow Control)/cl4_6_PrintReverseOfANumber.java
+++ C-4 Loops (Flow Control)/cl4_6_PrintReverseOfANumber.java
@@ -1,3 +1,24 @@
+/*
+ ! 4.6 Print REVERSE of a NUMBER:
+ *Example: 
+    int n= 10899
+ * OUTPUT: 99801
+ 
+todo If we want to GET the LAST DIGIT of a NUMBER: number%10 = LAST DIGIT will be there.
+todo If we want to REMOVE the LAST DIGIT of a NUMBER: number/10 =  LAST DIGIT will be REMOVED 
+ */
 public class cl4_6_PrintReverseOfANumber {
+  public static void main (String args []){
+
+    
+    int n= 10899;
+    while(n>0){
+      int lastDigit= n%10;
+      System.out.print(lastDigit);
+      n= n/10;
+      
+    }
+    System.out.println();
+  }
   
 }
